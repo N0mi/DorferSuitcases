631e38439972d01aee376e577e5181c518e6f016
Timofey Martynov <mart-tima@yandex.ru>
2020-11-21 14:12:57 +0500

    Init + Art

---
+++ Assets/Scripts/Core/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Core
+{
+    [CreateAssetMenu(fileName = "Country", menuName = "SuitcaseSystem/Country")]
+    public class Country : ScriptableObject
+    {
+        public string title;
+        public Image logo;
+        public List<Item> items;
+    }
+}
+
